890afccd63748a720b4b07211b867948cef270c8
arennera23 <88583364+arennera23@users.noreply.github.com>
2022-04-24 21:40:42 +0900

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ function App() {
           <div className="daily-forecast">Daily Forecast</div>
         </div>
       </div>
+      <p>
+        Open-sourced code by Aren Langeg on <a href="">GitHub.</a>
+        <a href="https://github.com/arennera23/react-app.git">GitHub.</a>
+      </p>
     </div>
   );
 }
